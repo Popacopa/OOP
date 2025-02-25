98be34a4a4fc4abf814e0e4549a68073ad85a095
Popacopa <paushakow@gmail.com>
2025-02-25 21:52:02 +0300

    comments

--- lab1/lab1.hpp
+++ lab1/lab1.hpp
@@ -5,11 +5,14 @@
 #include <sstream>
 #include <algorithm>
 
+// класс Worker - информация о работнике
 class Worker {
     public:
+    // конструктор
     Worker(std::string name = "none", std::string job = "none", int year = 2000)
     : name(name), job(job), year(year) {}
 
+    // оператор сравнения для сортировки
     bool operator<(const Worker& other) const {
         int len = std::min(name.length(), other.name.length());
         int i = 0;
@@ -19,6 +22,7 @@ class Worker {
         return name[i] < std::tolower(other.name[i]);
     }
 
+    // обновление данных от пользователя
     void updateFromConsole() {
         std::cout << "Введите имя работника: ";
         std::cin >> name;
@@ -29,6 +33,7 @@ class Worker {
         std::cin.ignore();
     }
 
+    // вывод информации
     void printInfo() const {
         //std::cout << "\n";
         std::cout << "\x1b[32mИмя:\x1b[32m " << name << "\n"
@@ -38,26 +43,32 @@ class Worker {
         std::cout << "\n";
     }
 
+    // геттеры
     std::string getName() const {return name;}
     std::string getJob() const {return job;}
     int getYear() const {return year;}
 
+    // сеттеры
     void setName(const std::string& name) { this->name = name; }
     void setJob(const std::string& job) { this->job = job; }
     void setYear(int year) { this->year = year; }
 
     private:
+    // поля класса
     std::string name;
     std::string job;
     int year; 
 };
 
+// структура Workers - управление информацией о работниках
 struct Workers {
 
+    // добавить работника
     void append(Worker& person) {
         data.push_back(person);
     }
 
+    // считать информацию из файла
     void ReadFromFile() {
         std::ifstream file("data.txt");
         if (!file.is_open()) {
@@ -80,6 +91,7 @@ struct Workers {
         
     }
     
+    // вывести информацию
     void printInfo() {
         if (data.empty()) {std::cout << "\x1b[31mрабочих нет!" << "\x1b[0m" << "\n";}
         for (int i = 0; i < data.size(); i++) {
@@ -87,20 +99,24 @@ struct Workers {
         }
     }
 
+    // обновить информацию
     void update(int& num) {
         num--;
         data[num].updateFromConsole();
     }
 
+    // удалить работника
     void remove(int& num) {
          num--;
          data.erase(data.begin() + num);
     }
 
+    // сортировать
     void sort() {
         std::sort(data.begin(), data.end());
     }
 
+    // поиск по имени
     void search_withname(std::string& name) {
         bool flag = 0;
         for (int i = 0; i < data.size(); i++) {
@@ -112,6 +128,7 @@ struct Workers {
         if (flag == 0) {std::cout << "\x1b[31mнет результатов!" << "\x1b[0m" << "\n";}
     }
 
+    // поиск по году
     void search_withyear(int& in) {
         bool flag = 0;
         for (int i = 0; i < data.size(); i++) {
@@ -123,6 +140,7 @@ struct Workers {
          if (flag == 0) {std::cout << "\x1b[31mнет результатов!" << "\x1b[0m" << "\n";}
     }
 
+    // поиск по должности
     void search_withjob(std::string& job) {
         bool flag = 0;
         for (int i = 0; i < data.size(); i++) {
@@ -134,5 +152,6 @@ struct Workers {
          if (flag == 0) {std::cout << "\x1b[31mнет результатов!" << "\x1b[0m" << "\n";}
     }
 
+    // вектор с информацией о работниках
     std::vector<Worker> data;
 };
